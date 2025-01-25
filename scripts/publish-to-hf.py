0b80a1d481630678232aab241b73a66c30d80a95
semantic-release <semantic-release>
2025-01-25 15:01:24 +0000

    0.1.3
    Automatically generated by python-semantic-release

--- scripts/publish-to-hf.py
+++ scripts/publish-to-hf.py
@@ -1,11 +1,25 @@
-from pgn_tokenizer import PGNTokenizer
+from pathlib import Path
 
-__version__ = "0.1.3"
+from transformers import PreTrainedTokenizerFast  # noqa: E402
 
-tokenizer = PGNTokenizer().tokenizer
+from pgn_tokenizer import __version__
+from pgn_tokenizer.constants import DATASET_NAME  # noqa: E402
+
+base_path = Path(__file__).parent.parent
+
+tokenizer_config_path = (
+    base_path / f"src/pgn_tokenizer/config/{DATASET_NAME}.json"
+).resolve()
+tokenizer_vocab_path = (
+    base_path / f"src/pgn_tokenizer/config/{DATASET_NAME}-vocab.json"
+).resolve()
+tokenzier_merges_path = (
+    base_path / f"src/pgn_tokenizer/config/{DATASET_NAME}-merges.txt"
+).resolve()
+
+tokenizer = PreTrainedTokenizerFast(tokenizer_file=str(tokenizer_config_path))
 
 tokenizer.push_to_hub(
-    repo_id="InterwebAlchemy/PGNTokenizer",
+    "InterwebAlchemy/PGNTokenizer",
     revision=__version__,
-    token=True,
 )
