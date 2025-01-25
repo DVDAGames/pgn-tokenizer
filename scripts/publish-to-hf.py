22b62eacd39a5044ac82c4ddccc3eb307bb47641
semantic-release <semantic-release>
2025-01-25 15:01:24 +0000

    0.1.3
    Automatically generated by python-semantic-release

--- scripts/publish-to-hf.py
+++ scripts/publish-to-hf.py
@@ -6,7 +6,6 @@ tokenizer = PGNTokenizer().tokenizer
 
 tokenizer.push_to_hub(
     repo_id="InterwebAlchemy/PGNTokenizer",
-    organization="InterwebAlchemy",
     revision=__version__,
-    use_auth_token=True,
+    token=True,
 )
